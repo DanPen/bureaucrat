db58eccfc150e63fa249615b017e04db5f81c94f
Daniel Pendergast <danielpendergast@icloud.com>
2016-05-11 20:29:15 -0500

    Remove api file

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var app = express();
 
 app.use(require('body-parser').json());
 
-var api = require('./api');
-app.use('/api', api);
 
 
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
