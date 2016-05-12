d3018f848805a1198a16d7264ba2622bcd4d34a7
Daniel Pendergast <danielpendergast@icloud.com>
2016-05-11 20:30:56 -0500

    Use CommonJS system

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
-var express = require('express');
-var app = express();
+module.exports = function() {
+  var express = require('express');
+  var app = express();
+  app.use('/node_modules', express.static(__dirname + '/node_modules'));
+  app.use(express.static(__dirname + '/public'));
 
-app.use(require('body-parser').json());
+  app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+  })
 
-
-
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
-
-app.use(express.static(__dirname + '/public'));
-
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
-})
-
-app.listen(3000);
+  app.listen(3000);
+}
