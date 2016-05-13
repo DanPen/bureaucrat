0a68756e74cce7519f052a918d227e733f62fae0
Daniel Pendergast <danielpendergast@icloud.com>
2016-05-13 14:40:52 -0500

    Add publishDate property

--- public/app/articles-mock.ts
+++ public/app/articles-mock.ts
@@ -1,12 +1,12 @@
 import {Article} from './article';
 
 export var ARTICLES: Article[] = [
-  {'id': 1, 'title': 'A story that will warm your heart1', 'content': 'there. done.'},
-  {'id': 2, 'title': 'A story that will warm your heart2', 'content': 'there. done.'},
-  {'id': 3, 'title': 'A story that will warm your heart3', 'content': 'there. done.'},
-  {'id': 4, 'title': 'A story that will warm your heart4', 'content': 'there. done.'},
-  {'id': 5, 'title': 'A story that will warm your heart5', 'content': 'there. done.'},
-  {'id': 6, 'title': 'A story that will warm your heart6', 'content': 'there. done.'},
-  {'id': 7, 'title': 'A story that will warm your heart7', 'content': 'there. done.'},
-  {'id': 8, 'title': 'A story that will warm your heart8', 'content': 'there. done.'},
+  {'id': 1, 'title': 'A story that will warm your heart1', 'content': 'there. done.', 'publishDate': new Date('Fri May 13 2016 14:38:14 GMT-0500 (CDT)')},
+  {'id': 2, 'title': 'A story that will warm your heart2', 'content': 'there. done.', 'publishDate': new Date('Fri May 14 2016 14:38:14 GMT-0500 (CDT)')},
+  {'id': 3, 'title': 'A story that will warm your heart3', 'content': 'there. done.', 'publishDate': new Date('Fri May 15 2016 14:38:14 GMT-0500 (CDT)')},
+  {'id': 4, 'title': 'A story that will warm your heart4', 'content': 'there. done.', 'publishDate': new Date('Fri May 16 2016 14:38:14 GMT-0500 (CDT)')},
+  {'id': 5, 'title': 'A story that will warm your heart5', 'content': 'there. done.', 'publishDate': new Date('Fri May 17 2016 14:38:14 GMT-0500 (CDT)')},
+  {'id': 6, 'title': 'A story that will warm your heart6', 'content': 'there. done.', 'publishDate': new Date('Fri May 18 2016 14:38:14 GMT-0500 (CDT)')},
+  {'id': 7, 'title': 'A story that will warm your heart7', 'content': 'there. done.', 'publishDate': new Date('Fri May 19 2016 14:38:14 GMT-0500 (CDT)')},
+  {'id': 8, 'title': 'A story that will warm your heart8', 'content': 'there. done.', 'publishDate': new Date('Fri May 20 2016 14:38:14 GMT-0500 (CDT)')},
 ];
