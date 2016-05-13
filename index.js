8964e75b6c157c24f68e0a6f55882cdce8c626b8
Daniel Pendergast <danielpendergast@icloud.com>
2016-05-13 14:40:15 -0500

    Fix package entry point

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+var path = require('path');
+
 module.exports = function() {
   var express = require('express');
   var app = express();
-  app.use('/node_modules', express.static(__dirname + '/node_modules'));
+  app.use('/node_modules', express.static(path.join(__dirname, '../')));
   app.use(express.static(__dirname + '/public'));
 
   app.get('*', (req, res) => {
